a94823b623fef409db80acc54ae17c86b8dd73d5
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-06-15 10:11:05 +0300

    Add a simple controller

---
+++ src/main/java/academy/learnprogramming/gradledemo/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package academy.learnprogramming.gradledemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class WelcomeController {
+	
+	@GetMapping("welcome")
+	public String welcome(Model model) {
+		model.addAttribute("message", "Hello Gradle");
+		log.info("welcome() called; model = {}", model);
+		return "welcome";
+	}
+}
